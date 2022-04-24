37e419b86dd0fb572e1f5a99ed29d54ad4f2449c
icecrimu <charles.banigon@gmail.com>
2022-04-24 16:18:48 +0800

    Primitive Types

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
-let message: string = "Hello World"
-console.log(message)
+let isPresent: boolean = false
+let magin: number = 66.6
+let hello: string = "world"
+
+let notDefined: undefined = undefined
+let notPresent: null = null
+
+let penta: symbol = Symbol("start")
+// let biggy: bigint = 24n
