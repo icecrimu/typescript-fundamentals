0bb518579e051278d9fb7c1baadf4507c97c0c5a
icecrimu <charles.banigon@gmail.com>
2022-07-17 10:12:20 +0800

    async await

--- src/index.ts
+++ src/index.ts
@@ -237,3 +237,16 @@ console.log(number) // 1234
 
 // Type Declaration
 console.log('Logged in user:', process.env.USER)
+
+// Async Await
+const delay = (ms: number) => new Promise(res => setTimeout(res, ms))
+const mainAsync = async () => {
+  await delay(1000)
+  console.log('1s')
+  await delay(1000)
+  console.log('2s')
+  await delay(1000)
+  console.log('3s')
+}
+
+mainAsync()
